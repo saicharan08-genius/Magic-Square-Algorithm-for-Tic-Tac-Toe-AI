c90b8f8d18ba3f529546ac8381bf21414921cd02
Saicharan Mugunthan <70827777+saicharan08-genius@users.noreply.github.com>
2021-02-03 13:17:40 +0530

    Update TicTacToeAI.py

--- TicTacToeAI.py
+++ TicTacToeAI.py
@@ -1,13 +1,7 @@
-#code under progress
-list1 = [1 , [2 , 4 , 5]]
-list2 = [2 , [1 , 3 , 5]]
-list3 = [3 , [2 , 5 , 6]]
-list4 = [4 , [1 , 5 , 7]]
-list5 = [5 , [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9]]
-list6 = [6 , [3 , 5 , 9]]
-list7 = [7 , [4 , 5 , 8]]
-list8 = [8 , [5 , 7 , 9]]
-list9 = [9 , [5 , 6 , 8]]
+human_input = [0 , 0 , 0 , 0]
+human_input_values = [0 , 0 , 0 , 0]
+human_input1 = [0 , 0 , 0 , 0 , 0]
+human_input1_values = [0 , 0 , 0 , 0 , 0]
 values = {
   1 : 2,
   2 : 7,
@@ -19,61 +13,42 @@ values = {
   8 : 3,
   9 : 8
 }
+values1 = {
+  2 : 1,
+  7 : 2,
+  6 : 3,
+  9 : 4,
+  5 : 5,
+  1 : 6,
+  4 : 7,
+  3 : 8,
+  8 : 9
+}
 print("Tic Tac Toe Board:\n")
-print("_|_|_")
-print(f"{list1[0]}|{list2[0]}|{list3[0]}")
-print("_|_|_")
-print(f"{list4[0]}|{list5[0]}|{list6[0]}")
-print("_|_|_")
-print(f"{list7[0]}|{list8[0]}|{list9[0]}")
-print("_|_|_")
-print(" | |")
+def printboard(game_board):
+  print(str(game_board[0])+'|'+str(game_board[1])+'|'+str(game_board[2]))
+  print(str(game_board[3])+'|'+str(game_board[4])+'|'+str(game_board[5]))
+  print(str(game_board[6])+'|'+str(game_board[7])+'|'+str(game_board[8]))
+board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+printboard(board)
+print("(Each number denote the position of the shell)\n")
+board_values = [2 , 7 , 6 , 9 , 5 , 1 , 4 , 3 , 8]
 a = 0
 b = 0
-c = 0
-d = 0
-human_input = [a , b  , c , d]
-a1 = 0
-b1 = 0
-c1 = 0
-d1 = 0
+def board_values(a):
+  board_value = int(values[a])
+  return board_value
+def board(b):
+  board_value1 = int(values1[b])
+  return board_value1
 bot_choice = 0
-human_input_values = [a1 , b1 , c1 , d1]
-human_xoro = str(input("Enter your choice. X or O:\n"))
+human_xoro = str(input("Hey! Do you choose X or O?:\n"))
 if human_xoro.lower() == "x":
-  human_input[0] = int(input("Enter position for X:\n"))
-  human_input_values[0] = values[human_input[0]]
+  human_input[0] = int(input("Enter Position for X:\n"))
   if human_input[0] != 5:
     bot_choice = 5
-    human_input[1] = int(input("Enter posiiton for X:\n"))
-    human_input_values[1] = values[human_input[1]]
-    if human_input[0] == list1[0]:
-      if human_input[1] in list1[1]:
-        pass
-      else:
-        pass
-    elif human_input[0] == list2[0]:
-      if human_input[1] in list2[1]:
-        pass
-      else:
-        pass
-    elif human_input[0] == list3[0]:
-      pass
-    elif human_input[0] == list4[0]:
-      pass
-    elif human_input[0] == list5[0]:
-      pass
-    elif human_input[0] == list6[0]:
-      pass
-    elif human_input[0] == list7[0]:
-      pass
-    elif human_input[0] == list8[0]:
-      pass
-    elif human_input[0] == list9[0]:
-      pass
   else:
-    bot_choice = 8
-    human_input[1] = int(input("Enter posiiton for X:\n"))
-    human_input_values[1] = values[human_input[1]]
+    bot_choice = 9 
+    
 else:
   pass
